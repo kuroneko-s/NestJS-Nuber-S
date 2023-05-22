f26ef442a5b5dbe6df87381563309195c3c921ff
kuroneko <drivespublic@gmail.com>
2023-05-23 08:47:44 +0900

    chap2 -F

--- src/restaurnants/dtos/create-restaurant.dto.ts
+++ src/restaurnants/dtos/create-restaurant.dto.ts
@@ -1,14 +1,19 @@
 import { InputType, ArgsType, Field } from '@nestjs/graphql';
+import { IsString, IsBoolean, Length } from 'class-validator';
 
 // @InputType() // arguments로써 Obejct에 전달받기 위함
 @ArgsType() // 분리된 값들을 arguments로 전달해줌
 export class CreateRestaurantDto {
   @Field(() => String)
+  @IsString()
   name: string;
 
   @Field(() => Boolean)
+  @IsBoolean()
   isGood?: boolean;
 
   @Field(() => String)
+  @IsString()
+  @Length(5, 10)
   ownerName: string;
 }
